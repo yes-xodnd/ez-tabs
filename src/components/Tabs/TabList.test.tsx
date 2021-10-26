b5974550c68ab72ed9ce7fe0afeee437bdf08a14
yes-xodnd <taew3167@gmail.com>
2021-10-26 16:54:05 +0900

    Update Test

--- src/components/Tabs/TabList.test.tsx
+++ src/components/Tabs/TabList.test.tsx
@@ -1,11 +1,17 @@
-import { render } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import TabList from "./TabList";
 import queryResult from 'src/dummy/tabs/queryResult';
 import api from 'src/api';
 
+let renderResult: RenderResult;
+
+beforeEach(() => {
+  renderResult = render(<TabList tabs={queryResult} addBookmarks={() => {}} />);
+})
+
 describe('TabList', () => {
   test('render tabs', () => {
-    const { getByText } = render(<TabList tabs={queryResult} />);
+    const { getByText } = renderResult;
     const titleList = queryResult.map(({ title }) => title as string);
     
     for (const title of titleList) {
@@ -14,7 +20,7 @@ describe('TabList', () => {
   });
 
   test('re-render tabs when removed', async () => {
-    const { queryByText } = render(<TabList tabs={queryResult} />);
+    const { queryByText } = renderResult;
     const { id, title } = queryResult[0];
 
     const target = queryByText(title as string);
