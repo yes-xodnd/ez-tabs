e7f3b77f411acdeddb2d623a942f78dbc95dc869
yes-xodnd <taew3167@gmail.com>
2021-10-29 14:29:51 +0900

    Add remove menu on dropdown

--- src/components/NodeList/DropDownContainer.tsx
+++ src/components/NodeList/DropDownContainer.tsx
@@ -1,24 +1,36 @@
-import Dropdown from "../UI/Dropdown";
+import { BookmarkNode } from "src/constants/types";
+import { useTypedDispatch } from "src/hooks";
+import { remove } from 'src/store/modules/bookmarksSlice';
+import Dropdown from "src/components/UI/Dropdown";
 
 interface Props {
+  node: BookmarkNode;
   handleClickRename: () => void;
   closeDropDown: () => void;
 }
 
-const DropDownContainer = ({ handleClickRename, closeDropDown }: Props) => {
-
+const DropDownContainer = ({ node, handleClickRename, closeDropDown }: Props) => {
+  const dispatch = useTypedDispatch();
+  
   const wrap = (fn: () => void) => () => {
     fn();
     closeDropDown();
-  }
-
-  const rename = {
-    title: '수정',
-    handleClick: wrap(handleClickRename)
   };
 
+  const createMenuItem = (title: string, handler: () => void) => {
+    return {
+      title,
+      handleClick: wrap(handler)
+    };
+  };
+
+  const menuItems = [
+    createMenuItem('수정', handleClickRename),
+    createMenuItem('삭제', () => dispatch(remove(node))),
+  ];
+
   return (
-    <Dropdown menuItems={[ rename ]} />
+    <Dropdown menuItems={menuItems} />
   );
 };
 
