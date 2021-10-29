f8d92becf6ba0398874aa45f74708cbf1287d13d
yes-xodnd <taew3167@gmail.com>
2021-10-30 07:21:27 +0900

    Update Theme

--- src/style/Theme.tsx
+++ src/style/Theme.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
 
-const defaultTheme = {
-  color: {
-    blue: 'royalblue',
+// const main = 'hsl(172, 79%, 29%)';
+const main = 'hsl(213, 81%, 52%)';
+
+const defaultTheme: DefaultTheme = {
+  colors: {
+    main: main,
+    mainLight: changeHSL(main, [0, 0, 10]),
+    mainDark: changeHSL(main, [0, 0, -10]),
+    danger: 'crimson',
+    hover: 'hsl(213, 20%, 97%)'
   }
 };
 
@@ -12,3 +19,12 @@ const Theme = ({ children }: { children: React.ReactNode }) => (
 );
 
 export default Theme;
+
+function changeHSL (hsl: string, options: [number, number, number]) {
+  const nums = hsl.match(/[0-9]+/g) as [string, string, string];
+  const next = nums
+    .map(v => Number(v))
+    .map((v, i) => v + options[i]);
+
+  return `hsl(${next[0]}, ${next[1]}%, ${next[2]}%)`;
+}
