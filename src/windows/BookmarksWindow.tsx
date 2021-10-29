ae5aed2e9f9f4a8ceb514379c6cd94b062d353c3
yes-xodnd <taew3167@gmail.com>
2021-10-30 02:46:03 +0900

    Refactor

--- src/windows/BookmarksWindow.tsx
+++ src/windows/BookmarksWindow.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { WindowTypes } from 'src/constants/types';
 import { WindowWrapper } from 'src/style';
-import { useWindow } from 'src/hooks';
+import { useIsActiveWindow } from 'src/hooks';
 
 import WindowHeader from 'src/components/UI/WindowHeader';
 import Toolbar from 'src/components/Bookmarks/BookmarksToolbar';
@@ -10,14 +10,14 @@ import NodeListContainer from 'src/components/NodeList/NodeListContainer';
 
 const BookmarksWindow = () => {
   const windowType: WindowTypes = 'BOOKMARKS';
-  const { isActive, hideWindow } = useWindow(windowType);
+  const isActive = useIsActiveWindow(windowType);
 
   return (
     <>
     {
       isActive && 
       <Wrapper>
-        <WindowHeader title={'북마크'} hideWindow={hideWindow} />
+        <WindowHeader title={'북마크'} windowType={windowType} />
         <Toolbar />
         <ContentContainer>
           <FolderTree />
