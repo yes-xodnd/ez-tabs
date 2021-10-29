d7fc33d5b008fdc3e55d7823e462700e263fd2c6
yes-xodnd <taew3167@gmail.com>
2021-10-30 01:57:46 +0900

    Add common styles

--- src/style/index.ts
+++ src/style/index.ts
@@ -2,8 +2,7 @@ import styled, { css } from 'styled-components';
 
 export const WindowWrapper = styled.div`
   flex-grow: 2;
-  max-width: 800px;
-  padding: 1rem;
+  min-width: 400px;
   border-radius: 5px;
   background-color: white;
   box-shadow: 0 0 5px lightgrey;
@@ -12,16 +11,46 @@ export const WindowWrapper = styled.div`
 
 export const customScroll = css`
   &::-webkit-scrollbar {
-    width: 1rem;
+    width: 0.5rem;
   }
 
   &::-webkit-scrollbar-thumb {
-    background-color: royalblue;
-    border-radius: 0.5rem;
+    background-color: darkgrey;
+
+    &:hover {
+      background-color: royalblue;
+    }
   }
 
   &::-webkit-scrollbar-track {
-    background-color: white;
-    border-radius: 0.5rem;
+    background-color: transparent;
   }
 `;
+
+export const ToolbarWrapper = styled.div`
+  display: flex;
+  /* justify-content: flex-end; */
+  gap: 5px;
+  padding: 5px 1rem;
+  border-top: 1px solid lightgrey;
+  border-bottom: 1px solid lightgrey;
+`;
+
+export const ToolbarButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  padding: 3px;
+  border-radius: 5px;
+  font-size: 0.8rem;
+  background-color: transparent;
+  border: none;
+  color: grey;
+  
+  &:hover {
+    cursor: pointer;
+    background: royalblue;
+    /* box-shadow: 1px 1px 2px grey; */
+    color: white;
+  }  
+`
