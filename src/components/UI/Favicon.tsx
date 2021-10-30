d41626d95360df64b4d85178546060fa4d1d2642
yes-xodnd <taew3167@gmail.com>
2021-10-31 03:32:30 +0900

    Add size props

--- src/components/UI/Favicon.tsx
+++ src/components/UI/Favicon.tsx
@@ -3,25 +3,27 @@ import styled from 'styled-components';
 interface Props {
   url?: string;
   snatcher?: boolean;
+  size?: string;
 }
 
 
-const Favicon = ({ url, snatcher }: Props) => {
+const Favicon = ({ url, snatcher, size = '16' }: Props) => {
 
   const getFavUrl = (url: string | undefined) => (!url || snatcher)
     ? `https://www.google.com/s2/favicons?domain=${url}`
     : url;
 
-  return <Wrapper url={getFavUrl(url)} />
+  return <Wrapper url={getFavUrl(url)} size={size} />
 };
 
 export default Favicon;
 
-const Wrapper = styled.div<{ url: string }>`
+const Wrapper = styled.div<{ url: string, size: string }>`
   background-image: url(${props => props.url});
-  background-size: contain;
-  max-width: 16px;
-  max-height: 16px;
+  background-size: cover;
+  background-repeat: none;
+  max-width: ${props => props.size + 'px'};
+  max-height: ${props => props.size + 'px'};
   width: 100%;
   height: 100%;
 `;
