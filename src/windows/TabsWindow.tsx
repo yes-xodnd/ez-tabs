38598f28a4d14a8a91ab1f69b31a58353810651c
yes-xodnd <taew3167@gmail.com>
2021-11-01 11:33:30 +0900

    Refactor

--- src/windows/TabsWindow.tsx
+++ src/windows/TabsWindow.tsx
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 import { WindowTypes } from 'src/constants/types';
-import { useIsActiveWindow } from 'src/hooks';
-import { WindowWrapper } from 'src/style';
+import { useIsActiveWindow, useToggle } from 'src/hooks';
+import { WindowWrapper, customScroll } from 'src/style';
 
 import WindowHeader from 'src/components/UI/WindowHeader';
 import Toolbar from 'src/components/Tabs/TabsToolbar';
-import TabListContainer from 'src/components/Tabs/TabListContainer';
+import TabsContainer from 'src/components/Tabs/TabsContainer';
 import ButtonAdd from 'src/components/Tabs/ButtonAdd';
 
 const TabsWindow = () => {
   const windowType: WindowTypes = 'TABS';
+    
+  const [ isListView, toggleView ] = useToggle(true);
   const isActive = useIsActiveWindow(windowType);
 
   return (
@@ -18,8 +20,12 @@ const TabsWindow = () => {
         isActive &&
         <Wrapper>
           <WindowHeader title={'탭'} windowType={windowType} />
-          <Toolbar />
-          <TabListContainer />
+          <Toolbar isListView={isListView} toggleView={toggleView} />
+
+          <ContentWrapper isListView={isListView}>
+            <TabsContainer isListView={isListView} />
+          </ContentWrapper>
+
           <ButtonAdd  />
         </Wrapper>
       }
@@ -33,3 +39,14 @@ const Wrapper = styled(WindowWrapper)`
   display: flex;
   flex-direction: column;
 `;
+
+const ContentWrapper = styled.div<{ isListView: boolean }>`
+  flex-grow: 2;
+  padding: 1rem;
+  overflow-y: auto;
+  border-bottom: 1px solid lightgrey;
+  background-color: ${props => props.isListView ? 'white' : props.theme.colors.hover};
+  z-index: 1;
+
+  ${ customScroll }
+`
