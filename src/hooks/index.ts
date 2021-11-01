352bdc0cb558e4b711bb7fb2a11c14359812e1dc
yes-xodnd <taew3167@gmail.com>
2021-11-01 11:33:08 +0900

    Add useToggle

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import { useTypedDispatch, useTypedSelector } from './redux';
 import { useFolderOpen, useCurrentFolder } from './FolderTree';
 import { useIsActiveWindow } from './Windows';
 
+const useToggle = (initState: boolean): [ boolean, () => void ] => {
+  const [ state, setState ] = useState(initState);
+  return [ state, () => setState(!state) ];
+}
+
+
 export {
+  useToggle,
   useTypedDispatch,
   useTypedSelector,
   useFolderOpen,
