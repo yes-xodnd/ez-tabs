6f6255d6a222be00a3b098afca58c3a5a608251f
yes-xodnd <taew3167@gmail.com>
2021-11-01 18:50:03 +0900

    Refactor addTabChangeListener
    wrapped it with once()

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import bookmarksDummy from 'src/dummy/bookmarks/api';
 import tabsDummy from 'src/dummy/tabs/api';
+import { once } from 'src/util';
 
 const api = (process.env.NODE_ENV === 'production')
   ? { 
@@ -11,12 +12,14 @@ const api = (process.env.NODE_ENV === 'production')
     tabs: tabsDummy
   };
 
-export const onTabsChange = (listener: () => void) => {
-  api.tabs.onRemoved.addListener(listener);
-  api.tabs.onCreated.addListener(listener);
-  api.tabs.onUpdated.addListener(listener);
-  api.tabs.onMoved.addListener(listener);
-  api.tabs.onReplaced.addListener(listener);
-}
+export const addTabsChangeListener = once(
+  (listener: () => void) => {
+    api.tabs.onRemoved.addListener(listener);
+    api.tabs.onCreated.addListener(listener);
+    api.tabs.onUpdated.addListener(listener);
+    api.tabs.onMoved.addListener(listener);
+    api.tabs.onReplaced.addListener(listener);
+  }
+);
 
 export default api;
