2c276a89f68d611b458bd369b8252049dd9b802e
yes-xodnd <taew3167@gmail.com>
2021-11-01 11:33:44 +0900

    Refactor

--- src/components/Tabs/TabsToolbar.tsx
+++ src/components/Tabs/TabsToolbar.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
 import { ToolbarWrapper } from "src/style";
-
 import ButtonToggleView from "./ButtonToggleVIew";
 import ButtonCheckAll from "./ButtonCheckAll";
 import ButtonRemoveCheckedTabs from "./ButtonRemoveCheckedTabs";
 
-const Toolbar = () => {
-  const [ isListView, setView ] = useState(true);
-  const toggleView = () => setView(!isListView);
+interface Props {
+  isListView: boolean;
+  toggleView: () => void;
+}
+
+const Toolbar = ({ isListView, toggleView }: Props) => {
 
   return (
     <ToolbarWrapper>
