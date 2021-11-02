866afe4cb4a747127e5870cf33cc80befd233ea9
yes-xodnd <taew3167@gmail.com>
2021-11-02 17:31:55 +0900

    Add label and style

--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -1,13 +1,35 @@
-import ToolbarButton from "src/components/UI/ToolbarButton";
-import { Search } from '@styled-icons/material-outlined';
+import { ChangeEventHandler } from "react";
+import styled from "styled-components";
 
-const SearchInput = ({ handleClick }: { handleClick: () => void}) => {
+interface Props {
+  handleInputChange: ChangeEventHandler;
+}
+
+const SearchInput = ({ handleInputChange }: Props) => {
+
   return (
-    <div>
-      <ToolbarButton title="검색" Icon={Search} handleClick={handleClick} />
-    </div>
+    <Wrapper>
+      <Label htmlFor="search-input">검색어를 입력하세요</Label>
+      <Input 
+        type="text"
+        id="search-input"
+        autoFocus
+        onChange={handleInputChange}
+        />
+    </Wrapper>
   );
 };
 
 export default SearchInput;
 
+const Wrapper = styled.div`
+  flex-grow: 2;
+`;
+
+const Label = styled.label`
+  font-size: 0;
+`;
+
+const Input = styled.input`
+  width: 100%;
+`;
