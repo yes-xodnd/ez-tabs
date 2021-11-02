1e11e1c5ff283a63c309adf97fdfc222c26071d0
yes-xodnd <taew3167@gmail.com>
2021-11-02 17:31:28 +0900

    Add SearchResult component

---
+++ src/components/Search/SearchResult.tsx
@@ -0,0 +1,22 @@
+import NodeListItem from 'src/components/NodeList/NodeListItem';
+import { BookmarkNode } from 'src/constants/types';
+import { customScroll } from 'src/style';
+import styled from 'styled-components';
+
+const SearchResult = ({ nodes }: { nodes: BookmarkNode[] }) => {
+
+  return (
+    <Wrapper>
+      { nodes.map(node => <NodeListItem node={node} key={node.id} />) }
+    </Wrapper>
+  );
+};
+
+export default SearchResult;
+
+const Wrapper = styled.div`
+  max-height: 100%;
+  padding: 1rem;
+  overflow-y: auto;
+  ${ customScroll }
+`;
