4a777f9e0f06bffff9037ab75fa3c38baf7dd0aa
yes-xodnd <taew3167@gmail.com>
2021-11-02 17:30:51 +0900

    Add debounce and throttle

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,8 +1,10 @@
+type F = (...args: any) => any;
+
 export const getHostname = (url: string) => {
   return new URL(url).hostname;
 };
 
-export const once = <T extends (...args: any) => any>(fn: T) => {
+export const once = <T extends F>(fn: T) => {
   let flag = false;
   
   return (...args: Parameters<T>): ReturnType<T> | void => {
@@ -12,3 +14,25 @@ export const once = <T extends (...args: any) => any>(fn: T) => {
     return fn(...args);
   };
 };
+
+export const throttle = <T extends F>(callback: T, t: number) => {
+  let timer: number | null = null;
+  
+  return (...args: Parameters<T>): void => {
+    if (timer) return;
+    
+    timer = window.setTimeout(() => { 
+      callback(...args); 
+      timer = null;
+    }, t);
+  };
+};
+
+export const debounce = <T extends F>(callback: T, t: number = 300) => {
+  let timer: number | null = null;
+
+  return (...args: Parameters<T>): void => {
+    if (timer) clearTimeout(timer);
+    timer = window.setTimeout(() => callback(...args), t);
+  }
+}
