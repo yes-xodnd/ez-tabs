e29b6d7fd82e32ac10dbb5f21a7aedd58ba4fa3b
yes-xodnd <taew3167@gmail.com>
2021-11-03 10:45:13 +0900

    Add create folder button

---
+++ src/components/Bookmarks/ButtonCreateFolder.tsx
@@ -0,0 +1,23 @@
+import { CreateNewFolder } from '@styled-icons/material-outlined';
+import { useTypedDispatch, useTypedSelector } from 'src/hooks';
+import { createFolder } from 'src/store/modules/bookmarksSlice';
+import ToolbarButton from "../UI/ToolbarButton";
+
+const ButtonAddFolder = () => {
+  const { currentFolderNodeId } = useTypedSelector(state => state.bookmarks);
+  const dispatch = useTypedDispatch();
+
+  const handleClick = () => {
+    dispatch(createFolder({
+      title: '새 폴더',
+      parentId: currentFolderNodeId,
+    }));
+  }
+
+  return (
+    <ToolbarButton title={'새 폴더'} Icon={CreateNewFolder} handleClick={handleClick} />
+  );
+};
+
+export default ButtonAddFolder;
+
