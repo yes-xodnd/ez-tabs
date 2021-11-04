27300fc73e4d279d95ec1d4bb8aeaef4d9872a93
yes-xodnd <taew3167@gmail.com>
2021-11-04 17:40:54 +0900

    Add WIndowWrapper component

---
+++ src/components/UI/WindowWrapper.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { WindowTypes } from "src/constants/types";
+import { useTypedDispatch, useTypedSelector } from "src/hooks";
+import { activateWindow } from "src/store/modules/intefaceSlice";
+
+interface Props {
+  windowType: WindowTypes;
+  children?: JSX.Element | JSX.Element[];
+}
+
+const WindowWrapper = ({ windowType, children }: Props) => {
+  const { visibleWindows } = useTypedSelector(state => state.interfaces);
+  const isVisible = visibleWindows.includes(windowType);
+  const dispatch = useTypedDispatch();
+
+  return (
+    <>
+    {
+      isVisible &&
+      <Wrapper onClick={() => { dispatch(activateWindow(windowType)); }}>
+        { children }
+      </Wrapper>
+    }
+    </>
+  );
+};
+
+export default WindowWrapper;
+
+export const Wrapper = styled.div`
+  flex-grow: 2;
+  min-width: 500px;
+  max-width: 800px;
+
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 5px;
+  background-color: white;
+  box-shadow: 2px 2px 10px lightgrey;
+  overflow: hidden;
+`;
