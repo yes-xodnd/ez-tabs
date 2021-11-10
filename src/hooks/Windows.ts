77025e0a9d7155b6914e2eaee3bcb28ff41ad7dd
yes-xodnd <taew3167@gmail.com>
2021-11-10 17:34:37 +0900

    Delete unused hook file

--- src/hooks/Windows.ts
+++
@@ -1,8 +0,0 @@
-import { useTypedSelector } from 'src/hooks';
-import { WindowTypes } from 'src/constants/types';
-
-export const useIsVisibleWindow = () => {
-  const { visibleWindows } = useTypedSelector(state => state.interfaces);
-
-  return (windowType: WindowTypes) => visibleWindows.includes(windowType);
-};
