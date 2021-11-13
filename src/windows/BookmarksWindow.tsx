f28f4a42bde739da9917891e97c14ccbcf16c201
yes-xodnd <taew3167@gmail.com>
2021-11-14 07:34:41 +0900

    Add view toggle and keyHandler

--- src/windows/BookmarksWindow.tsx
+++ src/windows/BookmarksWindow.tsx
@@ -1,21 +1,34 @@
 import styled from 'styled-components';
+import { useSearchKeyHandlers, useTypedSelector } from 'src/hooks';
 
 import WindowWrapper from 'src/components/UI/WindowWrapper';
 import WindowHeader from 'src/components/UI/WindowHeader';
 import Toolbar from 'src/components/Bookmarks/BookmarksToolbar';
 import FolderTree from 'src/components/FolderTree/FolderTree';
 import NodeListContainer from 'src/components/NodeList/NodeListContainer';
+import SearchResult from 'src/components/Search/SearchResult';
+
 
 const BookmarksWindow = () => {
+  const view = useTypedSelector(state => state.bookmarks.view);
+  const searchKeyHandlers = useSearchKeyHandlers();
+
+  const keyHandlers = view === 'SEARCH'
+    ? searchKeyHandlers
+    : {};
 
   return (
-    <WindowWrapper windowType={'BOOKMARKS'} >
-      <WindowHeader title='북마크' windowType={'BOOKMARKS'} />
+    <WindowWrapper windowType={'BOOKMARKS'} keyHandlers={keyHandlers}>
+      <WindowHeader title='북마크' windowType={'BOOKMARKS'}/>
       <Toolbar />
-      <ContentContainer>
-        <FolderTree />
-        <NodeListContainer />
-      </ContentContainer>
+      {
+        view === 'SEARCH'
+        ? <SearchResult />
+        : <ContentContainer>
+            <FolderTree />
+            <NodeListContainer />
+          </ContentContainer>
+      }
     </WindowWrapper>
   );
 };
