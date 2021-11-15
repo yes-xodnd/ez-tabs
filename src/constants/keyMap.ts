0ae675b916b3bce700a689791013f9ed37655572
yes-xodnd <taew3167@gmail.com>
2021-11-16 01:52:14 +0900

    Update KeyMap

--- src/constants/keyMap.ts
+++ src/constants/keyMap.ts
@@ -1,10 +1,17 @@
 export const tabsKeyMap = {
-  'MOVE_DOWN': 'ArrowDown',
-  'MOVE_UP': 'ArrowUp',
-  'CHECK': ' ',
-  'CLOSE_FOCUSED_TAB': 'Delete',
-  'CLOSE_CHECKED_TABS': 'Ctrl+Delete',
-  'SAVE_SNAPSHOT': 'Ctrl+Enter',
-  'MOVE_TOP': 'Home',
-  'MOVE_BOTTOM': 'End',
+  // List
+  MOVE_DOWN: 'ArrowDown',
+  MOVE_UP: 'ArrowUp',
+  MOVE_TOP: 'Home',
+  MOVE_BOTTOM: 'End',
+
+  // ListItem
+  CHECK: ' ',
+  OPEN_FOCUSED_TAB: 'Enter',
+  CLOSE_FOCUSED_TAB: 'Delete',
+
+  // Toolbar
+  CLOSE_CHECKED_TABS: 'Ctrl+Delete',
+  SAVE_SNAPSHOT: 'Ctrl+Enter',
+  CHECK_ALL: 'Ctrl+a'
 };
