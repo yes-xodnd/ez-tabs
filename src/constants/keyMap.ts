e29e066bf2a2ca71afa17ca71b13b426230bad03
yes-xodnd <taew3167@gmail.com>
2021-11-16 01:43:46 +0900

    Add tabs keyMap

---
+++ src/constants/keyMap.ts
@@ -0,0 +1,10 @@
+export const tabsKeyMap = {
+  'MOVE_DOWN': 'ArrowDown',
+  'MOVE_UP': 'ArrowUp',
+  'CHECK': ' ',
+  'CLOSE_FOCUSED_TAB': 'Delete',
+  'CLOSE_CHECKED_TABS': 'Ctrl+Delete',
+  'SAVE_SNAPSHOT': 'Ctrl+Enter',
+  'MOVE_TOP': 'Home',
+  'MOVE_BOTTOM': 'End',
+};
