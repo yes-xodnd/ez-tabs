6468bb590014741fd62296e222441555e7166fad
yes-xodnd <taew3167@gmail.com>
2021-11-15 16:11:18 +0900

    Add tree key handler

--- src/windows/BookmarksWindow.tsx
+++ src/windows/BookmarksWindow.tsx
@@ -7,15 +7,17 @@ import Toolbar from 'src/components/Bookmarks/BookmarksToolbar';
 import FolderTree from 'src/components/FolderTree/FolderTree';
 import NodeListContainer from 'src/components/NodeList/NodeListContainer';
 import SearchResult from 'src/components/Search/SearchResult';
+import { useTreeKeyHandlers } from 'src/hooks/bookmarks';
 
 
 const BookmarksWindow = () => {
   const view = useTypedSelector(state => state.bookmarks.view);
   const searchKeyHandlers = useSearchKeyHandlers();
+  const treeKeyHandlers = useTreeKeyHandlers();
 
   const keyHandlers = view === 'SEARCH'
     ? searchKeyHandlers
-    : {};
+    : treeKeyHandlers;
 
   return (
     <WindowWrapper windowType={'BOOKMARKS'} keyHandlers={keyHandlers}>
