13a0e4299afe82dc3929c8266cd2c97c7c456f84
yes-xodnd <taew3167@gmail.com>
2021-11-16 01:43:29 +0900

    Add keyHandlers

--- src/hooks/Tabs.ts
+++ src/hooks/Tabs.ts
@@ -1,9 +1,10 @@
-import { activateFocusedTab, closeCheckedTabs, toggleCheck } from "src/store/modules/tabsSlice";
+import { activateFocusedTab, closeCheckedTabs, toggleCheck, toggleCheckAll,moveFocusIndex, closeFocusTab, toggleCheckFocused, clearCheck, setFocusIndexEnd } from "src/store/modules/tabsSlice";
 import { useTypedDispatch } from ".";
 import { closeTab } from "src/store/modules/tabsSlice";
 import { throttle } from "src/util";
 import { useMemo } from "react";
-import { moveFocusIndex, closeTabHotkey, toggleCheckFocused } from 'src/store/modules/tabsSlice';
+import { createFromTabs } from "src/store/modules/bookmarksSlice";
+import { openWindow } from "src/store/modules/windowsSlice";
 
 export const useToggleCheckTab = (id: number | undefined) => {
   const dispatch = useTypedDispatch();
@@ -15,24 +16,40 @@ export const useCloseTab = (id: number | undefined) => {
   const dispatch = useTypedDispatch();
   
   return throttle(() => { id && dispatch(closeTab(id)); }, 1000);
-}
+};
 
 export const useCloseCheckedTabs = () => {
   const dispatch = useTypedDispatch();  
 
   return throttle(() => { dispatch(closeCheckedTabs()) }, 1000);
-}
+};
+
+export const useSnapshotTabs = () => {
+  const dispatch = useTypedDispatch();
+  
+  return () => {
+    dispatch(createFromTabs());
+    dispatch(clearCheck());
+    dispatch(openWindow('BOOKMARKS'));
+  };
+};
 
 export const useTabsKeyHandlers = () => {
   const dispatch = useTypedDispatch();
+  const snapshotTabs = useSnapshotTabs();
 
   const keyHandlers = useMemo(() =>({
-    ArrowUp: () => dispatch(moveFocusIndex(-1)),
-    ArrowDown: () => dispatch(moveFocusIndex(1)),
+    'ArrowUp': () => dispatch(moveFocusIndex(-1)),
+    'ArrowDown': () => dispatch(moveFocusIndex(1)),
     ' ': () => dispatch(toggleCheckFocused()),
-    Enter: () => dispatch(activateFocusedTab()),
-    Delete: () => dispatch(closeTabHotkey())
-  }), [ dispatch ]);
+    'Enter': () => dispatch(activateFocusedTab()),
+    'Delete': () => dispatch(closeFocusTab()),
+    'Ctrl+a': () => dispatch(toggleCheckAll()),
+    'Ctrl+Delete': () => dispatch(closeCheckedTabs()),
+    'Ctrl+Enter': snapshotTabs,
+    'Home': () => dispatch(setFocusIndexEnd('START')),
+    'End': () => dispatch(setFocusIndexEnd('END')),
+  }), [ dispatch, snapshotTabs ]);
 
   return keyHandlers;
 }
