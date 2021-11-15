2582991f2c847001beb55a2649d9541d7db07e04
yes-xodnd <taew3167@gmail.com>
2021-11-16 00:35:00 +0900

    Use autoFocus hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
-import { useInitData, useHotkeys, useCombinedKeyHandlers } from './hooks';
+import { useInitData, useHotkeys, useCombinedKeyHandlers, useAutoFocusRef } from './hooks';
 
 import BookmarksWindow from './windows/BookmarksWindow';
 import TabsWindow from './windows/TabsWindow';
 import Menubar from './components/Menubar/Menubar';
+import { useEffect, useRef } from 'react';
 
 function App() {
   useInitData();
   const keyHandlers = useCombinedKeyHandlers();
   const handleKeyDown = useHotkeys(keyHandlers);
+  const ref = useAutoFocusRef<HTMLDivElement>();
 
   return (
-    <Wrapper tabIndex={-1} onKeyDown={handleKeyDown} >
+    <Wrapper tabIndex={-1} onKeyDown={handleKeyDown} ref={ref}>
       <Menubar />
       <TabsWindow />
       <BookmarksWindow />
@@ -23,4 +25,8 @@ export default App;
 
 const Wrapper = styled.div`
   height: 600px;
+  overflow: hidden;
+  &:focus {
+    outline: 1px solid black;
+  }
 `;
