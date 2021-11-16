3ff815c042a3dd03b150bd76713255e549d24ab4
yes-xodnd <taew3167@gmail.com>
2021-11-17 01:52:40 +0900

    Update imports

--- src/components/Bookmarks/ButtonRemove.tsx
+++ src/components/Bookmarks/ButtonRemove.tsx
@@ -1,5 +1,5 @@
 import { useTypedDispatch } from "src/hooks";
-import { removeChecked } from "src/store/modules/bookmarksSlice";
+import { removeChecked } from "src/store/modules/nodeListSlice";
 import { Delete } from '@styled-icons/material-outlined';
 import ToolbarButton from 'src/components/UI/ToolbarButton';
 
