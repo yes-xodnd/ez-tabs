1fd08851aff70a49925ffef23f269c0967afbb54
yes-xodnd <taew3167@gmail.com>
2021-12-07 22:08:42 +0900

    Add thunkApiConfig Type

--- src/store/index.ts
+++ src/store/index.ts
@@ -22,5 +22,10 @@ const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type Dispatch = typeof store.dispatch;
+export type ThunkApiConfig = { 
+  state: RootState,
+  dispatch: Dispatch,
+  extra: { api: typeof api }
+}
 
 export default store;
